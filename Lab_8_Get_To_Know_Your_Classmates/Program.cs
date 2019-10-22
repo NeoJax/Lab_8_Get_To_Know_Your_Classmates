aff23e9e5f61e29e261ccae24420752f53ea7fda
Johnathon Lubaway <NeoJax@gmail.com>
2019-10-22 19:34:45 -0400

    Finishing touches on Lab 8

--- Lab_8_Get_To_Know_Your_Classmates/Program.cs
+++ Lab_8_Get_To_Know_Your_Classmates/Program.cs
@@ -10,42 +10,19 @@ namespace Lab_8_Get_To_Know_Your_Classmates
 {
     class Program
     {
+
+        // I am sorry for the length of this program
         static void Main(string[] args)
         {
+            // Call the Classmate info Method
             GetClassMateInfo();
         }
 
-        public static string GetUserInput(string message)
-        {
-            Console.Write(message + " ");
-            string input = Console.ReadLine();
-            Console.WriteLine();
-            return input;
-        }
-
-        public static int ParseRange(int min, int max, string message)
-        {
-            try
-            {
-                int number = int.Parse(GetUserInput(message));
-
-                if (number >= min && number <= max)
-                {
-                    return number;
-                }
-                else
-                {
-                    return ParseRange(min, max, $"Please enter a valid number. ({min} - {max})  ");
-                }
-            }
-            catch
-            {
-                return ParseRange(min, max, message);
-            }
-        }
-
+        // This method defines and sets up a class with info about them
         public static void GetClassMateInfo()
         {
+
+            // Sets up a two-dimensional array that represents the class and their interests
             string[,] classmates = new string[6, 4];
 
             // Names of the students
@@ -80,29 +57,145 @@ namespace Lab_8_Get_To_Know_Your_Classmates
             classmates[4, 3] = "Chemistry";
             classmates[5, 3] = "Drawing";
 
-            int currentStudent = ParseRange(1, 6, "Welcome to the class of Third Street Elementary " +
-                              "School, which student would you like to know more" +
-                              "about? (Enter a number from 1-6)");
-            Console.Write($"Student {currentStudent} is {classmates[currentStudent, 0]}." +
-                              $"What would you like to know about them? (Enter \"food\"," +
-                              $"\"subject\", or \"hobby\")");
-            string favorites = GetUserInput($"What would you like to know about them? " +
-                               $"(Enter \"food\", \"subject\", or \"hobby\")");
-            if (favorites == "food")
+            // This try catch makes sure everything is in range of the given numbers
+            int currentStudent = -1;
+
+            string check = ParseLife("Would you rather use a students \"number\" or \"name\"");
+
+
+            currentStudent = ParseStudent(check, classmates);
+
+            string favorites = ValidateInput();
+
+            // This prints out the respective students favorite
+            switch (favorites)
             {
-                Console.WriteLine(classmates[currentStudent, 1]);
-            }
-            else if (favorites == "subject")
-            {
-                Console.WriteLine(classmates[currentStudent, 2]);
-            }
-            else if (favorites == "hobby")
-            {
-                Console.WriteLine(classmates[currentStudent, 3]);
+                case "food":
+                    Console.WriteLine($"{classmates[currentStudent, 0]}'s favorite food is " + classmates[currentStudent, 1]);
+                    break;
+                case "subject":
+                    Console.WriteLine($"{classmates[currentStudent, 0]}'s favorite subject is " + classmates[currentStudent, 2]);
+                    break;
+                case "hobby":
+                    Console.WriteLine($"{classmates[currentStudent, 0]}'s favorite hobby is " + classmates[currentStudent, 3]);
+                    break;
             }
 
+            Console.WriteLine();
+            GetContinue();
         }
 
+        // This determines whether someone wants to look up a student by name or number
+        public static int ParseStudent(string check, string[,] classmates)
+        {
+            int currentStudent = 0;
+
+            // This catches issues if there is an out of range exception
+            try
+            {
+                switch (check)
+                {
+                    case "number":
+                        currentStudent = ParseInteger("Welcome to our class. " +
+                          "Which student would you like to know more about? " +
+                          $"(Enter a number from 1-6)") - 1;
+
+                        break;
+                    case "name":
+                        currentStudent = ParseName("Welcome to our class. " +
+                             "Which student would you like to know more about? " +
+                             "(Enter a name)");
+                        break;
+                    default:
+                        break;
+                }
+                Console.Write($"Student {currentStudent + 1} is {classmates[currentStudent, 0]}. ");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Please enter a valid number.");
+                currentStudent = ParseStudent(check, classmates);
+            }
+
+            return currentStudent;
+        }
+
+        // This method prompts the user to get some information
+        public static string GetUserInput(string message)
+        {
+            Console.Write(message + " ");
+            string input = Console.ReadLine();
+            Console.WriteLine();
+            return input;
+        }
+
+        // This method takes information from the user and makes sure it is an integer and is in range
+        public static int ParseInteger(string message)
+        {
+            // This try catch makes sure that whatever the user inputs is an integer
+            try
+            {
+                return int.Parse(GetUserInput(message));
+            }
+            catch (FormatException)
+            {
+                return ParseInteger(message);
+            }
+        }
+
+        // This method makes sure the user calls the correct name
+        public static int ParseName(string message)
+        {
+            string name = GetUserInput(message);
+            int student = -1;
+            switch (name)
+            {
+                case "Vince":
+                    student = 0;
+                    break;
+                case "Spinelli":
+                    student = 1;
+                    break;
+                case "Mikey":
+                    student = 2;
+                    break;
+                case "T.J.":
+                    student = 3;
+                    break;
+                case "Gretchen":
+                    student = 4;
+                    break;
+                case "Gus":
+                    student = 5;
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid name.");
+                    Console.WriteLine();
+                    return ParseName(message);
+            }
+            return student;
+        }
+
+        // This method makes sure the answer to the favorites question is valid
+        public static string ValidateInput()
+        {
+            // Prompts the user to pick a topic they want to know about a classmate
+            string favorites = GetUserInput($"What would you like to know about them? " +
+                                            $"(Enter \"food\", \"subject\", or \"hobby\")");
+            switch (favorites)
+            {
+                case "food":
+                    return "food";
+                case "subject":
+                    return "subject";
+                case "hobby":
+                    return "hobby";
+                default:
+                    return ValidateInput();
+            }
+        }
+
+        // This method checks to see if the user wants to run through the program again
         public static void GetContinue()
         {
             string input = GetUserInput("Do you want to continue? (y/n)");
@@ -119,6 +212,22 @@ namespace Lab_8_Get_To_Know_Your_Classmates
                 GetContinue();
             }
 
+        }
+
+        // This method determines whether this program should be longer or not
+        public static string ParseLife(string message)
+        {
+            string check = GetUserInput(message);
+            switch (check)
+            {
+                case "name":
+                    return check;
+                case "number":
+                    return check;
+                default:
+                    Console.WriteLine("Please return \"name\" or \"number\".\n");
+                    return ParseLife(message);
+            }
         }
     }
 }
