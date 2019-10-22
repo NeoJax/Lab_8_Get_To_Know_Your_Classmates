c83a43c613001d60f675c31173742aac3fab99a5
Johnathon Lubaway <NeoJax@gmail.com>
2019-10-22 15:06:08 -0400

    Initializing the repo for Lab 8

---
+++ Lab_8_Get_To_Know_Your_Classmates/Program.cs
@@ -0,0 +1,59 @@
+﻿// ===============================
+// AUTHOR     : Jonathan Lubaway
+// CREATE DATE: October 22nd, 2019
+// PURPOSE    : Write a program that will recognize invalid user inputs when the
+//              user requests information about students in a class
+// ===============================
+using System;
+
+namespace Lab_8_Get_To_Know_Your_Classmates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Classmate: Favorite Food, Hometown, Favorite Hobby 
+        }
+
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static int ParseInteger(string message)
+        {
+            try
+            {
+                return int.Parse(GetUserInput(message));
+            }
+            catch
+            {
+                return ParseInteger(message);
+            }
+        }
+
+        public static void GetClassMateInfo()
+        {
+
+        }
+
+        public static void GetContinue()
+        {
+            string input = GetUserInput("Do you want to continue? (y/n)");
+            if (input == "y")
+            {
+                GetClassMateInfo();
+            }
+            else if (input == "n")
+            {
+                Console.WriteLine("Thank you!");
+            }
+            else
+            {
+                GetContinue();
+            }
+
+        }
+    }
+}
